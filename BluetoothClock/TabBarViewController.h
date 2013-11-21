ae2622f94781d921f363e6998911e9cb12486037
JimmyHo <jimmyho@JimmyHotekiMacBook-Pro.local>
2013-11-21 17:50:37 +0800

    init

---
+++ BluetoothClock/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  BluetoothClock
+//
+//  Created by JimmyHo on 2013/11/13.
+//  Copyright (c) 2013年 JimmyHo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
---
+++ BluetoothClock/TabBarViewController.m
@@ -0,0 +1,38 @@
+//
+//  TabBarViewController.m
+//  BluetoothClock
+//
+//  Created by JimmyHo on 2013/11/13.
+//  Copyright (c) 2013年 JimmyHo. All rights reserved.
+//
+
+#import "TabBarViewController.h"
+
+@interface TabBarViewController ()
+
+@end
+
+@implementation TabBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
